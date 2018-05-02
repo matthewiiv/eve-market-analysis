2ef2e7bb5139dc006b25a002165e54d0b04476bd
Matt Sharp <matthewsharp123@gmail.com>
2018-05-02 21:32:50 +0100

    finish main item and region api calls and storage - also add db query and best haul route for given region and investment

--- src/eve_api/apiFunction.js
+++ src/eve_api/apiFunction.js
@@ -5,6 +5,7 @@ const redis = require('redis');
 const regionIds = require('../data').regionIds;
 const typeIds = require('../data').typeIds;
 
+let regionStatus = [];
 var client = redis.createClient();
 
 function constructURL(root, params) {
@@ -62,7 +63,7 @@ var getTypeOrdersByRegion = (callback) => {
 
 var writeTypeRegionOrdersArrToDb = (callback) => {
   return (regionId, arr, arrIndex) => {
-    try {
+    if (arr[arrIndex] !== undefined && arr[arrIndex][0] !== undefined) {
       let typeId = arr[arrIndex][0].type_id;
       let orders = arr[arrIndex];
       var buyOrders = orders.filter((order) => {
@@ -77,12 +78,16 @@ var writeTypeRegionOrdersArrToDb = (callback) => {
         } else {
           callback(err, res)
         }
-      })
-    } catch (e) {
+      });
+    } else {
       if(arrIndex < arr.length - 1) {
         writeTypeRegionOrdersArrToDb(callback)(regionId, arr, arrIndex + 1)
       } else {
-        callback(e)
+        if (arr[arrIndex] == undefined) {
+          callback(`items ${arr}`, 'failed')
+        } else if (arr[arrIndex][0] == undefined) {
+          callback(`item index ${arrIndex} ${arr[arrIndex]}`, 'failed')
+        }
       }
     }
   }
@@ -96,9 +101,12 @@ function getAllAPIOrders(regionIds, regionIndex, attempt) {
       if(results[0].error !== undefined) {
         console.log(`region ${regionId} fucked up`)
         if (attempt < 6) {
-          getAllAPIOrders(regionIds, regionIndex, attempt + 1)
+          console.log(`retrying..`)
+          setTimeout(function(){
+            getAllAPIOrders(regionIds, regionIndex, attempt + 1)
+          }, 2000);
         } else {
-          if(regionIndex < regionIds.length) {
+          if(regionIndex < regionIds.length - 1) {
             getAllAPIOrders(regionIds, regionIndex + 1, 1)
           } else {
             console.log("finished")
@@ -136,6 +144,8 @@ function getAllAPIOrders(regionIds, regionIndex, attempt) {
                 for (var i = 0; i < resultsNestTwo.length; i++) {
                   if(resultsNestTwo[i].value) {
                     itemsOrders.push(resultsNestTwo[i].value.body);
+                  } else if (resultsNestTwo[i].error) {
+                    //console.log("item errored", resultsNestTwo[i].error)
                   }
                 }
                 let itemsOrdersFlat = [].concat(...itemsOrders);
@@ -151,11 +161,12 @@ function getAllAPIOrders(regionIds, regionIndex, attempt) {
                   }
                   console.log(`region ${regionId} write successful`);
                   writeTypeRegionOrdersArrToDb((err, res) => {
-                    console.log(`region ${regionId} orders write ${res}`, `errors ${err}`)
-                    if(regionIndex < regionIds.length) {
+                    console.log(`region ${regionId} orders write ${res}`, `errors ${err}`);
+                    if(regionIndex < regionIds.length - 1) {
                       getAllAPIOrders(regionIds, regionIndex + 1, 1)
                     } else {
-                      console.log("finished")
+                      console.log("done")
+                      getAllAPIOrders(regionIds, 0, 1)
                     }
                   })(regionId, itemsOrders, 0);
                 });
@@ -167,178 +178,5 @@ function getAllAPIOrders(regionIds, regionIndex, attempt) {
     }
   );
 }
-
-// const startTime = Date.now();
-//
-// let typeBuyOrders = [];
-// let typeSellOrders = [];
-//
-
-//
-// var getOrders = (callback) => {
-//   return (typeId, regionId) => {
-//     var start = Date.now();
-//     apiSends.push(start)
-//     let itemRoot = `https://esi.tech.ccp.is/latest/markets/${regionId}/orders/`;
-//     var itemParams = {
-//       "datasource": "tranquility",
-//       "order_type": "all",
-//       "page": 1,
-//       "type_id": typeId
-//     };
-//     const itemUrl = constructURL(itemRoot, itemParams);
-//     request
-//     .get(itemUrl)
-//     .then((res) => {
-//       console.log(Date.now()-start)
-//       callback(null, res.body)
-//     })
-//     .catch((err) => {
-//       callback(err, "error");
-//     })
-//   }
-// }
-//
-// function getAllAPIOrders(typeId) {
-//   // var start = Date.now();
-//   async.parallel(
-//     [
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000001')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000002')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000003')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000004')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000005')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000006')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000007')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000008')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000009')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000010')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000011')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000012')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000013')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000014')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000015')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000016')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000017')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000018')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000019')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000020')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000021')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000022')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000023')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000025')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000027')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000028')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000029')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000030')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000031')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000032')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000033')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000034')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000035')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000036')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000037')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000038')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000039')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000040')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000041')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000042')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000043')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000044')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000045')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000046')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000047')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000048')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000049')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000050')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000051')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000052')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000053')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000054')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000055')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000056')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000057')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000058')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000059')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000060')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000061')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000062')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000063')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000064')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000065')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000066')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000067')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000068')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'10000069')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000001')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000002')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000003')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000004')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000005')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000006')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000007')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000008')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000009')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000010')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000011')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000012')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000013')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000014')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000015')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000016')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000017')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000018')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000019')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000020')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000021')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000022')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000023')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000024')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000025')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000026')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000027')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000028')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000029')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000030')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000031')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000032')}),
-//       async.reflect((callback) => {getOrders(callback)(typeId,'11000033')})
-//
-//     ],
-//     (err, results) => {
-//       if(err){
-//         // console.log("err", Date.now() - start)
-//         // console.log(err.status);
-//         // if (typeIds.indexOf(typeId) < typeIds.length - 1) {
-//         //   getAllAPIOrders(typeIds[typeIds.indexOf(typeId) + 1]);
-//         // } else {
-//         //   getAllAPIOrders(0);
-//         // }
-//       } else {
-//         // console.log(Date.now() - start)
-//         var successfulOrders = [];
-//         for (var i = 0; i < results.length; i++) {
-//           if(results[i].value) {
-//             successfulOrders.push(results[i].value);
-//           }
-//         }
-//         var orders = [].concat(...successfulOrders);
-//         var buyOrders = orders.filter((order) => {
-//           return order.is_buy_order;
-//         });
-//         var sellOrders = orders.filter((order) => {
-//           return !order.is_buy_order;
-//         });
-//         client.hmset(`type:${typeId}`,"buy", JSON.stringify(buyOrders), "sell", JSON.stringify(sellOrders), "lastUpdated", JSON.stringify(Date.now()), (err, response) => {
-//           console.log(`write error ` + err, `${typeId} write ` + response, (Date.now() - startTime) * (1 / (typeIds.indexOf(typeId) / typeIds.length)) / 3600000);
-//           if (typeIds.indexOf(typeId) < typeIds.length - 1) {
-//             getAllAPIOrders(typeIds[typeIds.indexOf(typeId) + 1]);
-//           } else {
-//             getAllAPIOrders(0);
-//           }
-//         })
-//       }
-//     });
-//   }
 
 module.exports = getAllAPIOrders;
--- src/index.js
+++ src/index.js
@@ -8,23 +8,114 @@ const testParallel = require('./eve_api/apiFunction');
 const typeIds = require('./data').typeIds;
 const regionIds = require('./data').regionIds;
 
-
+const bankBalance = 1000000;
 
 var redis = require("redis")
 var client = redis.createClient();
 //testFunction();
 
-testParallel(regionIds, 0);
+testParallel(regionIds, 0, 1);
 
 // if you'd like to select database 3, instead of 0 (default), call
 // client.select(3, function() { /* ... */ });
 
 client.on("error", function (err) {
-    console.log("Error " + err);
+  console.log("Error " + err);
 });
 
+//function getRegionOrders
+client.hget("region:10000001", "sell", (err,reply) => {
+  if (err) {
+    console.log(err);
+  }
+  let bestItemPrices = [];
+  let items = JSON.parse(reply);
+  for (var i = 0, len = items.length; i < len; i++) {
+    if (bestItemPrices[items[i].type_id] !== undefined) {
+      if(items[i].price < bestItemPrices[items[i].type_id].price) {
+        bestItemPrices[items[i].type_id] = items[i];
+      }
+    } else {
+      bestItemPrices[items[i].type_id] = items[i];
+    }
+  }
+  bestItemPrices = bestItemPrices.filter(n => true)
+  let typeIds = bestItemPrices;
+  let bestBuyPrices = [];
+  getAndCompareOtherRegionBuyPrices(regionIds, 0, typeIds, 0, bestBuyPrices, (bestBuyPrices) => {
+    // Decide what to do with bestBuyPrices array and best Item Prices to get best hauling routes
+    let orderedDeals = [];
+    for (var i = 0, len = bestItemPrices.length; i < len; i++) {
+      let bestBuyItem = bestBuyPrices[bestItemPrices[i].type_id]
+      if (bestBuyItem !== undefined) {
+        let maxTransactionVolume = Math.min(bestBuyItem.volume_remain, bestItemPrices[i].volume_remain)
+        let maxRealProfit = (bestBuyItem.price - bestItemPrices[i].price) * maxTransactionVolume
+        let profitMargin = bestBuyItem.price / bestItemPrices[i].price;
+        let purchasePrice = bestItemPrices[i].price;
+
+        let totalPurchasePrice = purchasePrice;
+        let j = 1;
+
+        while(totalPurchasePrice < bankBalance && j <= maxTransactionVolume) {
+          j++;
+          totalPurchasePrice = j * purchasePrice;
+        }
 
 
+        bestBuyItem.profitMargin = profitMargin;
+        bestBuyItem.maxRealProfit = maxRealProfit;
+        bestBuyItem.purchase_price = bestItemPrices[i].price;
+        bestBuyItem.maxTransactionVolume = maxTransactionVolume;
+        bestBuyItem.maxTransactionAmount = maxTransactionVolume  * bestBuyItem.purchase_price;
+        bestBuyItem.numberToBuy = (j-1);
+        bestBuyItem.totalPurchaseValue = (j-1) * purchasePrice;
+        bestBuyItem.actualProfit = ((j-1) * purchasePrice * profitMargin) - bestBuyItem.totalPurchaseValue;
+        orderedDeals.push(bestBuyItem);
+      }
+    }
+    orderedDeals.sort(function(a, b) {
+      return b.actualProfit - a.actualProfit;
+    });
+    console.log(orderedDeals.slice(0, 4))
+  })
+})
+
+function getAndCompareOtherRegionBuyPrices(regionIds, regionIndex, typeIds, typeIdIndex, bestBuyPrices, callback) {
+  let typeId = typeIds[typeIdIndex].type_id;
+  client.hget(`type:${typeId}:region:${regionIds[regionIndex]}`, "buy", (err, reply) => {
+    if (err || reply == null) {
+      if (regionIndex < regionIds.length - 1) {
+        getAndCompareOtherRegionBuyPrices(regionIds, regionIndex + 1, typeIds, typeIdIndex, bestBuyPrices, callback)
+      } else {
+        if (typeIdIndex < typeIds.length - 1) {
+          getAndCompareOtherRegionBuyPrices(regionIds, 0, typeIds, typeIdIndex + 1, bestBuyPrices, callback)
+        } else {
+          callback(bestBuyPrices)
+        }
+      }
+    } else {
+      let itemBuyOrders = JSON.parse(reply);
+      for (var i = 0, len = itemBuyOrders.length; i < len; i++) {
+        if (bestBuyPrices[itemBuyOrders[i].type_id] !== undefined) {
+          if(itemBuyOrders[i].price > bestBuyPrices[itemBuyOrders[i].type_id].price) {
+            bestBuyPrices[itemBuyOrders[i].type_id] = itemBuyOrders[i];
+          }
+        } else {
+          bestBuyPrices[itemBuyOrders[i].type_id] = itemBuyOrders[i];
+        }
+      }
+      if (regionIndex < regionIds.length - 1) {
+        getAndCompareOtherRegionBuyPrices(regionIds, regionIndex + 1, typeIds, typeIdIndex, bestBuyPrices, callback)
+      } else {
+        if (typeIdIndex < typeIds.length - 1) {
+          getAndCompareOtherRegionBuyPrices(regionIds, 0, typeIds, typeIdIndex + 1, bestBuyPrices, callback)
+        } else {
+          callback(bestBuyPrices)
+        }
+      }
+    }
+  });
+}
 
 // client.hmset("user:1000", "username", "antirez", "birthyear", "1977", "verified", "1", function (err, replies) {
 //     console.log(err, replies);
@@ -35,38 +126,38 @@ client.on("error", function (err) {
 // });
 
 const server = Hapi.server({
-    port: 3000,
-    host: 'localhost'
+  port: 3000,
+  host: 'localhost'
 });
 
 server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: (request, h) => {
-        request.logger.info('In handler %s', request.path);
-        return `Hello, ${encodeURIComponent(request.params.name)}!`;
-    }
+  method: 'GET',
+  path: '/{name}',
+  handler: (request, h) => {
+    request.logger.info('In handler %s', request.path);
+    return `Hello, ${encodeURIComponent(request.params.name)}!`;
+  }
 });
 
 const init = async () => {
 
-    await server.register(require('inert'));
+  await server.register(require('inert'));
 
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: (request, h) => {
-            return h.file('./public/index.html');
-        }
-    });
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: (request, h) => {
+      return h.file('./public/index.html');
+    }
+  });
 
-    await server.start();
-    console.log(`Server running at: ${server.info.uri}`);
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
 };
 
 process.on('unhandledRejection', (err) => {
-    console.log(err);
-    process.exit(1);
+  console.log(err);
+  process.exit(1);
 });
 
 init();
